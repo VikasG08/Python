aabcdcc33d57983acf3f622df27ab3e2086e1625
VikasG08 <76769421+VikasG08@users.noreply.github.com>
2020-12-30 19:06:42 +0530

    Update base.py
    updated

--- vegefoods/settings/base.py
+++ vegefoods/settings/base.py
@@ -31,7 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_celery_results',
+    
 ]
 
 ROOT_URLCONF = 'vegefoods.urls'
